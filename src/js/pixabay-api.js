b8d53b992d30515d4e41b1cfb3588e9a8efd161a
MykolaNovokhatko <143940513+MykolaNovokhatko@users.noreply.github.com>
2024-04-01 20:21:17 +0100

    hw12

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,13 +1,19 @@
-export function fetchUsers(value) {
-    const searchParams = new URLSearchParams({
-      key: '43103378-6af8ee9eb079e11667ecfffdc',
-      q: value,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: true,
-    });
-    const url = `https://pixabay.com/api/?${searchParams}`;
-    return fetch(url).then(response => {
-      return response.json();
-    });
+import axios from 'axios';
+export async function fetchUsers(value, currentPage) {
+  const searchParams = new URLSearchParams({
+    key: '43103378-6af8ee9eb079e11667ecfffdc',
+    q: value,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: '15',
+    page: currentPage,
+  });
+  const url = `https://pixabay.com/api/?${searchParams}`;
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch {
+    console.log(error);
   }
+}
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -2,18 +2,18 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-export function renderUsers(hits, userList, loader) {
+export function renderUsers(hits, userList, loader, btnLoadMore) {
   if (hits.length <= 0) {
     loader.classList.add('is-hidden');
-    throw new Error(
-      iziToast.show({
-        title: 'Error',
-        message:
-          'Sorry, there are no images matching your search query. Please try again!',
-        backgroundColor: '#ef4040',
-        position: 'topRight',
-      })
-    );
+    btnLoadMore.classList.add('is-hidden');
+    iziToast.show({
+      title: 'Error',
+      message:
+        'Sorry, there are no images matching your search query. Please try again!',
+      backgroundColor: '#ef4040',
+      position: 'topRight',
+    });
+    return;
   }
   const markup = hits
     .map(hit => {
@@ -31,11 +31,17 @@ export function renderUsers(hits, userList, loader) {
     })
     .join('');
   userList.insertAdjacentHTML('beforeend', markup);
-
   const lightbox = new SimpleLightbox('.gallery a', {
     captionsData: `alt`,
     captionDelay: 250,
   });
+
   loader.classList.add('is-hidden');
+  btnLoadMore.classList.remove('is-hidden');
   lightbox.refresh();
+}
+export function getParam() {
+  const elem = document.querySelector('.cards');
+  const elements = elem.getBoundingClientRect();
+  return elements.height;
 }
